4e5b3b2c81901760190e0346fceff62365edf063
Carlos Del Real <carloshdelreal@gmail.com>
2020-01-16 22:54:07 -0500

    finished doctor profile pages

--- src/components/doctorProfile.jsx
+++ src/components/doctorProfile.jsx
@@ -1,9 +1,88 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, Route, NavLink } from 'react-router-dom';
 import BackCaretWhite from '../images/backCaretWhite.png';
 import locationWhite from '../images/locationWhite.png';
 import callWhite from '../images/callWhite.png';
 import profileImg from '../images/doctorImage.jpg';
 import messageWhite from '../images/messageWhite.png';
+import mapImage from '../images/map.png';
+
+const DoctorInfo = () => {
+  return (
+    <div className="doctorProfile__doctorInfo">
+      <h5>Education</h5>
+      <ul>
+        <li>
+          MD - General Medicine|College Name
+        </li>
+        <li>
+          MBBS-Nephrology|More info
+        </li>
+      </ul>
+
+      <h5>Specialization</h5>
+      <ul>
+        <li>
+          Consultation Physician
+        </li>
+        <li>
+          Internal Medicine
+        </li>
+      </ul>
+
+      <h5>Services Offered</h5>
+      <ul>
+        <li>
+          Peritoneal Dialysis
+        </li>
+        <li>
+          Kidney Stone treatment
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const ClinicInfo = () => {
+  return (
+    <div className="doctorProfile__clinicInfo">
+      <h5>Santa Ana Clinic</h5>
+      <p>Here the direction and more info about the clinic</p>
+      <img src={mapImage} alt="locationMap" />
+    </div>
+  );
+};
+
+const Feedback = () => {
+  return (
+    <div className="doctorProfile__feedback">
+      <div>
+        <p className="doctorProfile__feedback--date">10 days ago</p>
+        <h5>happyUser123</h5>
+        <p className="doctorProfile__feedback--post">
+          doctor is very kind and Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Quas, et possimus, repellendus
+          consequatur perspiciatis maxime qui, iure at praesentium
+          consectetur vel! At ut adipisci officiis ipsam delectus
+          aliquid vel earum.
+        </p>
+      </div>
+      <hr />
+      <div>
+        <p className="doctorProfile__feedback--date">10 days ago</p>
+        <h5>happyUser123</h5>
+        <p className="doctorProfile__feedback--post">
+          doctor is very kind and Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Quas, et possimus, repellendus
+          consequatur perspiciatis maxime qui, iure at praesentium
+          consectetur vel! At ut adipisci officiis ipsam delectus
+          aliquid vel earum.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 const DoctorProfile = ({ match }) => {
   return (
@@ -12,7 +91,9 @@ const DoctorProfile = ({ match }) => {
       <div className="doctorProfile container">
         <div className="doctorProfile__nav row justify-content-between">
           <div className="col-6 text-left">
-            <img src={BackCaretWhite} alt="back caret" />
+            <Link to="/">
+              <img src={BackCaretWhite} alt="back caret" />
+            </Link>
           </div>
           <div className="doctorProfile__location col-6">
             <div>
@@ -68,10 +149,46 @@ const DoctorProfile = ({ match }) => {
             </div>
           </div>
         </div>
-
+      </div>
+      <div className="container">
+        <div className="doctorProfile__navInfo row">
+          <div className="col-4">
+            <NavLink to={match.url} activeStyle={{ color: 'red' }}>
+              Doctor's Info
+            </NavLink>
+          </div>
+          <div className="col-4">
+            <NavLink to={`${match.url}/email`} activeStyle={{ color: 'red' }}>
+              Clinic's Info
+            </NavLink>
+          </div>
+          <div className="col-4">
+            <NavLink to={`${match.url}/address`} activeStyle={{ color: 'red' }}>
+              Feedback
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      <div className='container'>
+        <Route exact path={match.url} component={DoctorInfo} />
+        <Route path={`${match.url}/email`} component={ClinicInfo} />
+        <Route path={`${match.url}/address`} component={Feedback} />
+      </div>
+      <div className="container">
+        <div className="doctorProfile__bookAppointment row justify-content-center">
+          <div className="col-10">
+            <button className="btn btn-block doctorProfile__bookButton" type="button">Book Appointment</button>
+          </div>
+        </div>
       </div>
     </div>
   );
+};
+
+DoctorProfile.propTypes = {
+  match: PropTypes.shape({
+    url: PropTypes.string,
+  }).isRequired,
 };
 
 export default DoctorProfile;
